337a1bc1edc1f3002f076eed1c7994c4e051a752
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2020-07-07 12:49:03 +1000

    Documentation added

--- packages/server-graphql/src/index.js
+++ packages/server-graphql/src/index.js
@@ -1,5 +1,131 @@
+/**
+ * @module Schema
+ *
+ * @description Use declarative schemas to generate database models and CRUD graphql.
+ */
+
 import lodash from "lodash";
 const { omit, defaults } = lodash;
+
+/**
+ * @typedef {object} Config
+ *
+ * @property {object} [mapKnexType]
+ *    Provides mappings for Schema types into knex column types.
+ *
+ * <pre>// defaults
+ * ID -> integer
+ * Int -> integer
+ * String -> text
+ * DateTime -> timestamp
+ * Boolean -> boolean</pre>
+ *
+ * @property {object} [mapGraphType]
+ *    Provides mappings for Schema types into alternate GraphQL types.
+ *    Although Schema types are primarily compatible graph types, it provides
+ *    a chance to remap types that, for example, might not have custom scalars
+ *    defined yet, but are ultimately rendered as a String|Int|etc.
+ *
+ * <pre>// defaults
+ * DateTime -> string</pre>
+ *
+ * @property {object} [mapKnexTypeSelect]
+ *    Provides a chance for additional selector args to be added to
+ *    `knex(table).select("*", ...args)` statements when resolvers fetch data.
+ *    Motivated by cases where the certain columns used custom postgis geometry
+ *    types and the data needed to be select/casted into JSON instead of it's
+ *    raw binary type.
+ *
+ * <pre>// no defaults</pre>
+ */
+
+/**
+ * @typedef {object} Schema
+ *    Descriptor used to generate database tables and graphql typedefs and
+ *    resolvers.
+ *
+ * @property {string} name
+ *    Entity name for database table and graph typedefs.
+ *
+ * @property {object} fields
+ *    Field definitions for the database columns and graph typedefs.
+ *
+ * @property {string} fields.type
+ *    Graph type (String|Boolean|etc.) or Schema name (User|etc.).
+ *    This value is mapped to a knex database type via `config.mapKnexType` is
+ *    this field will be a database column.
+ *    If used with relationship, this value can be a Schema name for the
+ *    generated resolver to resolve the data for this field.
+ *
+ * @property {boolean} [fields.virtual=false]
+ *    If true, this field will not have a corresponding database column.
+ *    It will only exist as a graph entity key.
+ *    Use this for generated/virtual resolved values etc.
+ *
+ * @property {string} [fields.column=null]
+ *    Use this value to change the database column name used for this field.
+ *    All generated relationships and resolvers will automatically resolve this
+ *    field's name to access the database column specified here instead.
+ *
+ * @property {boolean|string} [fields.relationship=false]
+ *    Configures this field to resolve to a relationship to another entity.
+ *    If set to true, the relationship will default to use `${field.type}.id`
+ *    when querying the remote table via this field's type. A different column
+ *    can be used if passed as a string instead of `true`.
+ *    If `many` is not specified, this field will be considered a database column
+ *    and a foreign key will be set using the `Table.column` value determined.
+ *    If `many` is true, this field will be considered a virtual field.
+ *
+ * @property {boolean} [fields.many=false]
+ *    If `true`, typedefs use `[]` and generated resolvers will use arrays.
+ *    If `many` is used with a relationship field, this field is considered
+ *    virtual.
+ *
+ * @property {boolean} [fields.nullable=true]
+ *    If `false`, adds the `NOT NULL` constraint for the database column, and
+ *    adds the `!` not nullable modifier to generated graph typedefs for this
+ *    schema.
+ *
+ * @property {boolean} [fields.private=false]
+ *    If `true`, does not expose this field on graph schema.
+ *    Setting `setter` and `getter` to `false` has the same effect.
+ *
+ * @property {boolean} [fields.primary=false]
+ *    Reserved `id` primary key column, which is automatically added to this
+ *    schema when generated, and must only be used for one field.
+ *    If `true`, resolves the database column type to "increments".
+ *
+ * @property {boolean} [fields.unique=false]
+ *    If `true`, adds unique constraint for this field's database column.
+ *
+ * @property {*} [fields.default]
+ *    If set, applies db column default for field.
+ *
+ * @property {boolean|string|GraphQL.Resolver} [fields.getter=true]
+ *    Modifies the generated resolver for getting this field's value.
+ *    If `true`, returned value is `obj[field.name]` (or `field.column` if set).
+ *    If `string`, returned value is `obj[field.getter|string]`.
+ *    If `GraphQL.Resolver`, returned value is the result of this resolver
+ *    function, called with standard resolver parameters:
+ *    `GraphQL.Resolver(parent, args, context, info)`.
+ *    Has no effect if `virtual` is true.
+ * @property {boolean|string|GraphQL.Resolver} [fields.setter=true]
+ *    Modifies the generated resolver for setting this field's value.
+ *    If `true`, returned value is `obj[field.name]` (or `field.column` if set).
+ *    If `string`, value
+ *    If `Resolver` function, will be called will Resolver args, and uses return
+ *      as new value for create/update mutations.
+ *    Has no effect if `virtual` is true.
+ *
+ * @property {object} [constraints={}]
+ *    Allows database table-level constraints to be set for fields/columns.
+ *
+ * @property {string[][]} [constraints.unique=[]]
+ *    An array of unique constraint sets of columns referenced by schema name
+ *    (not by `column` alias!). An example value of e.g. `[["userA", "userB"]]`
+ *    will ensure that no other database row can have the same two values set.
+ *
+ */
 
 const configDefault = {
   mapKnexType: {
@@ -15,61 +141,49 @@ const configDefault = {
   mapKnexTypeSelect: {},
 };
 
-// TODO: update schema api using jsdocs format
-// TODO: update schema with new flattened definition layout in User
-/**
- * Schema API
- *  name: string (required) table/graph entity type name
- *  fields: object (required) Object of [field name]: [field options]
- *    * primary key "id" is added automatically
- *    * createdAt and updatedAt property is added automatically also
- *      * defaults to knex.fn.now() on creation
- *
- *    type: string (required) graph type, is cast for matching knex database type
- *      * if "Relationship", no db column created for field (like isVirtual)
- *    primary: bool (false) if true, set db column to use primary key "increments"
- *      * only allowed for one column (id)
- *    default: any (undefined) if set, applies db column default for field
- *    references: string (undefined) if set, applies foreign key, as: "Table.column"
- *    private: bool (false), if true, does not add to graph schema
- *    isVirtual: bool (false) if true, does not create db column for field
- *      * only adds to graph schema, for virtualised/generated values
- *    isNotNullable: bool (false) if true, adds notNullable db constraint for field
- *      * also, adds ! not nullable to graph typedefs
- *    isUnique: bool (false) if true, adds unique constraint for db column to table
- *    join: object (required if type = Relationship), otherwise ignored
- *      type: string (required) schema name of related join
- *      many: bool (false) set graph to use [type] instead, for result array
- *      setter: bool|resolver (false), method invoked to correctly assign new value
- *      getter: bool|resolver (false), method invoked to correctly fetch/join values
- *      args: object (undefined) Of [arg name]: [arg graph type]
- *  constraints: object (undefined) Object of db table constraints
- *    unique: array<array> (undefined) List of unique constraint sets as lists
- */
-
 const fieldDefaults = {
-  // REQUIRED
-  type: undefined, // String (db/graph)
-  // OPTIONAL
-  column: null, // String|null (db)
-  relationship: false, // Boolean|String (db)
-  nullable: true, // Boolean (db/graph)
-  private: false, // Boolean (graph)
-  virtual: false, // Boolean (db)
-  primary: false, // Boolean (db)
-  unique: false, // Boolean (db)
-  default: undefined, // Any|null (db)
-  setter: true, // Boolean|String|Function (resolver)
-  getter: true, // Boolean|String|Function (resolver)
+  type: undefined,
+  column: null,
+  many: false,
+  relationship: false,
+  nullable: true,
+  private: false,
+  virtual: false,
+  primary: false,
+  unique: false,
+  default: undefined,
+  setter: true,
+  getter: true,
 };
 
-export const Parse = (configOverride) => {
+/**
+ * @function
+ * @static
+ * @param {Config} config
+ * @returns {SchemaParser}
+ *    Schema parsing function.
+ */
+export const Schema = (configOverride) => {
   const config = configOverride ? configOverride(configDefault) : configDefault;
   const { mapKnexType, mapGraphType, mapKnexTypeSelect } = config;
 
   const getKnexType = (type) => mapKnexType[type];
   const getGraphType = (type) => mapGraphType[type] || type;
 
+  /**
+   * @function
+   * @name SchemaParser
+   * @param {Schema} schema
+   *    Input schema to be parsed.
+   * @param {Object} ioc
+   *    Object with references to knex and an ResolverError handler.
+   * @param {Knex} ioc.knex
+   *    Reference to initialised knex instance.
+   * @param {function} ioc.ResolverError
+   *    Reference to an error constructor used for resolver errors.
+   *    (i.e. ApolloError) Called with `new ResolverError(message, code)`.
+   *
+   */
   return (schema, ioc) => {
     const { knex, ResolverError } = ioc;
 
--- packages/server-graphql/src/remote.js
+++ packages/server-graphql/src/remote.js
@@ -1,23 +1,71 @@
-import fetch from "node-fetch";
+/**
+ * @module Remote
+ */
+
 import graphql from "graphql";
 const { print } = graphql;
 import graphqlTools from "graphql-tools";
-const { introspectSchema, makeExecutableSchema, makeRemoteExecutableSchema, mergeSchemas } = graphqlTools;
+const {
+  introspectSchema,
+  makeExecutableSchema,
+  makeRemoteExecutableSchema,
+  mergeSchemas,
+} = graphqlTools;
 
-export const Executor = ({ uri, fetch }) => async ({ document, variables }) => {
-  const query = print(document);
-  const fetchResult = await fetch(uri, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query, variables })
-  });
-  return fetchResult.json();
-};
+/**
+ * Creates a GraphQL Executor for use with GraphQL Remote Executable Schemas.
+ *
+ * @function
+ * @static
+ * @param {object} config
+ * @param {string} config.uri
+ *    Target GraphQL API. (e.g. http://localhost:4000/graphql)
+ * @param {(function|object)} [config.context={}]
+ *    Callback that receives the execution context to be able to manipulate the
+ *    fetch function. Currently only adding additional headers is supported. If
+ *    passed an object, it is used as-is to provide manipulations to the fetch
+ *    function.
+ * @param {object} [config.context.headers={}]
+ *    Object of [header]:[value] pairs to add to the fetch request.
+ * @param {Fetch} config.fetch
+ *    Fetch implementation. (i.e. node-fetch)
+ *
+ * @returns {GraphQL.Executor}
+ */
+export function Executor({ uri, context, fetch }) {
+  return async (operation) => {
+    const {
+      headers = {}
+    } = (
+      typeof context === "function"
+        ? context(operation.context)
+        : context
+    ) || {};
 
-export async function RemoteSchema(uri) {
-  const executor = Executor({ uri, fetch });
+    const { document, variables } = operation;
+    const query = print(document);
+    const result = await fetch(uri, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...(headers || {}),
+      },
+      body: JSON.stringify({ query, variables })
+    });
+
+    return result.json();
+  };
+}
+
+/**
+ * Creates a GraphQL Remote Executable Schema.
+ *
+ * @function
+ * @static
+ * @param {GraphQL.Executor} executor
+ * @returns {GraphQL.RemoteExecutableSchema}
+ */
+export async function RemoteExecutableSchema(executor) {
   const schema = await introspectSchema(executor);
   const schemaExecutable = makeRemoteExecutableSchema({ schema, executor });
   return schemaExecutable;
