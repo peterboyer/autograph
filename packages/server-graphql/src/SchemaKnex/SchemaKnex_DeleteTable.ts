f5de09420f25c7ca8a586c59c427c132ee3e3dbf
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2020-08-27 11:27:36 +1000

    knex delete table only if exists

--- packages/server-graphql/src/SchemaKnex/SchemaKnex_DeleteTable.ts
+++ packages/server-graphql/src/SchemaKnex/SchemaKnex_DeleteTable.ts
@@ -4,10 +4,15 @@ export default function DeleteTable(ioc: IIOC) {
   const { knex } = ioc;
 
   return function (model: IModel) {
-    return async function (): Promise<void> {
+    return async function (): Promise<null | false> {
       const { name } = model;
+
+      if (!(await knex.schema.hasTable(name))) {
+        return false;
+      }
+
       await knex.schema.dropTable(name);
-      return;
+      return null;
     };
   };
 }
