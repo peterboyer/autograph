9e7064c5fa1903a9dce1fe05cb1a506466181141
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2020-09-22 15:24:16 +1000

    add documentation for typedefs

--- packages/server-schema/src/SchemaGraphQL/SchemaGraphQL.TypeDefs.ts
+++ packages/server-schema/src/SchemaGraphQL/SchemaGraphQL.TypeDefs.ts
@@ -11,7 +11,7 @@ const { print, parse } = graphql;
 const clean = (source: string) => print(parse(source));
 
 export default function TypeDefs(ioc: IIOC) {
-  const { mapType } = ioc;
+  const { mapType, limitDefault, limitMaxDefault } = ioc;
 
   function getGraphType(type: string) {
     return mapType.get(type) || type;
@@ -125,24 +125,53 @@ export default function TypeDefs(ioc: IIOC) {
         id: ID!
         ${graphFieldsTypeDefsInputPartial}
       }
+
       ${typeDefs?.Root || ""}
     `);
 
     const Query = `
+      # Accepts an \`id\` to return it's corresponding \`${name}\`. Throws if
+      # not found.
       ${name}(id: ID!): ${name}!
+
+      # Accepts a combination of one or many options to paginate through many
+      # \`${name}\` items.
+      # - \`cursor\`, used from a previous request to the get page of items. If
+      # specified, all other arguments are ignored.
+      # - \`order\`, used to order items (e.g. "fieldA:asc", "fieldB:desc")
+      # - \`filters\`, a map of filters and their values used to conditionally
+      # filter items. See \`${name}ManyFilters\` for available filters and value
+      # types.
+      # - \`limit\`, specify how many items per page
+      # (default: \`${limitDefault}\`, max: \`${limitMaxDefault}\`)
+      #
+      # Returns \`${name}ManyResult\` with a \`cursor\` value (to be used in a
+      # subsequent request to fetch the next page of items, \`null\` if no more
+      # items), \`total\` (to denote how many items exist), and \`items\` of
+      # \`${name}\`.
       ${name}_many(
         cursor: String
         order: String
         filters: ${name}ManyFilters
         limit: Int
       ): ${name}ManyResult!
+
       ${typeDefs?.Query || ""}
     `;
 
     const Mutation = `
+      # Accepts \`data\` as an array of \`${name}Input\` to persist, and
+      # returns those new items.
       ${name}_create(data: [${name}Input!]!): [${name}!]!
+
+      # Accepts \`data\` as an array of \`${name}InputPartial\` to update items
+      # by the given \`{ id }\` field, and returns those updated items.
       ${name}_update(data: [${name}InputPartial!]!): [${name}!]!
+
+      # Accepts \`ids\` as an array of \`ID\`s and returns those \`ID\`s after
+      # deletion.
       ${name}_delete(ids: [ID!]!): [ID!]!
+
       ${typeDefs?.Mutation || ""}
     `;
 
